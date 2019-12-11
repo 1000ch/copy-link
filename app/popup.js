47fdfbe4bff340fa26ba06e8b8e69796034538a3
1000ch <shogo.sensui@gmail.com>
2019-12-11 23:50:10 +0900

    feat: implement copy

--- app/popup.js
+++ app/popup.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded',() => {
-  console.log('popup.js');
+document.addEventListener('DOMContentLoaded', () => {
+  const text = document.querySelector('input');
+  const plain = document.querySelector('#plain');
+  const html = document.querySelector('#html');
+  const markdown = document.querySelector('#markdown');
+
+  chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  }, tabs => {
+    const { title, url } = tabs[0];
+
+    plain.addEventListener('click', () => {
+      text.value = `${title} ${url}`;
+      text.select();
+      document.execCommand('copy');
+    });
+
+    html.addEventListener('click', () => {
+      text.value = `<a href="${url}">${title}</a>`;
+      text.select();
+      document.execCommand('copy');
+    });
+
+    markdown.addEventListener('click', () => {
+      text.value = `[${title}](${url})`;
+      text.select();
+      document.execCommand('copy');
+    });
+  });
 });
